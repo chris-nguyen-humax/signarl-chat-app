a503b74daa567db858a3033fde350d6d28f36107
hieuhappyit <minhhieuit28@gmail.com>
2020-12-29 23:32:23 +0700

    init code

---
+++ Chris.ChatApp/Chris.ChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chris.ChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
